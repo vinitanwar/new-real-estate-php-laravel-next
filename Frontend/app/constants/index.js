3558f2df3c13e90ea27590b14f9b7b5e1b5a682e
vinitanwar <125426162+vinitanwar@users.noreply.github.com>
2025-02-05 19:41:52 +0530

    miness

---
+++ Frontend/app/constants/index.js
@@ -0,0 +1,32 @@
+
+
+export const navLink = [
+    {
+      id: 0,
+      label: 'Home',
+      href: '/',
+      Key: 'home'
+    },
+    {
+      id: 1,
+      label: 'Listing',
+      href: '/projects',
+      Key: 'listing'
+    },
+    {
+      id: 2,
+      label: 'Property',
+      href: '/properties',
+      Key: 'property'
+    },
+    {
+      id: 3,
+      label: 'Blog',
+      href: '/blog',
+      Key: 'blog'
+    }
+  ];
+  export const apiLink = 'https://prakarsh123.in/api/v1'
+  export const storageLink = 'https://prakarsh123.in/storage'
+
+  // /storage/$
---
+++ Frontend/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  
+    trailingSlash: true,
+    // output: "export", 
+    images: { unoptimized: true },
+}
+
+export default nextConfig;
