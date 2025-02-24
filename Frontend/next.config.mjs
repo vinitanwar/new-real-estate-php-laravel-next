a08e0701506c8891fb47bef266b18b0f50fcd58e
vinitanwar <125426162+vinitanwar@users.noreply.github.com>
2025-02-24 15:13:19 +0530

    nwfwrfw4fr

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
     images: {
          unoptimized: true ,
         domains: [
